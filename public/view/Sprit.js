a24b62d7e91403114c90b06c2ee704374c39800a
Leonardo Andriotti <leonardoandriotte@hotmail.com>
2016-09-09 14:29:07 -0300

    update

---
+++ public/view/Sprit.js
@@ -0,0 +1,17 @@
+function Sprite(x,y,largura,altura){
+   this.x = x;
+   this.y = y;
+   this.largura = largura;
+   this.altura = altura;
+
+   this.desenha = function(xCanvas,yCanvas,img){
+        
+		objeto = new Image();
+		objeto.src = img;
+        ctx.drawImage(objeto,this.x,this.y,this.largura,this.altura,xCanvas,yCanvas,this.largura,this.altura);
+   }
+}
+
+var bg = new Sprite(0,0,600,600);
+var boneco = new Sprite(0,0,60,102);
+var obstaculo = new Sprite(0,0,60,101);
